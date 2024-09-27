b34acbf2fd602ae6b7c2aac681a0400535dfbcf9
mati-gif <matiassoria7700@gmail.com>
2024-09-27 14:05:11 -0300

    añadiendo la primary key a la clase client

--- src/main/java/mindhub/VoyagerRestaurante/models/Client.java
+++ src/main/java/mindhub/VoyagerRestaurante/models/Client.java
@@ -2,9 +2,16 @@ package mindhub.VoyagerRestaurante.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Client {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String fistName;
 }
