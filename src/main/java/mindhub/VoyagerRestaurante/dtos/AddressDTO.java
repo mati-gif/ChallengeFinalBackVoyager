3f7684ef0fbf12e87fcb56ffc23d24692044b410
LuzMieres <mieresluz5@gmail.com>
2024-10-04 01:29:53 -0300

    Agregué controller de adress método para crear direcciones dependiendo el tipo de casa, metood para traer todos los adressDTO y otro método para los adressDTO del cliente logueado

--- src/main/java/mindhub/VoyagerRestaurante/dtos/AddressDTO.java
+++ src/main/java/mindhub/VoyagerRestaurante/dtos/AddressDTO.java
@@ -1,12 +1,7 @@
 package mindhub.VoyagerRestaurante.dtos;
 
 import mindhub.VoyagerRestaurante.models.Adress;
-import mindhub.VoyagerRestaurante.models.Client;
 import mindhub.VoyagerRestaurante.models.TypeHome;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class AddressDTO {
     private Long id;
@@ -17,8 +12,12 @@ public class AddressDTO {
     private TypeHome typeHome;
     private int floorNumber;
     private String aparmentNumber;
-//    private List<ClientDTO> clients;
 
+    // Constructor por defecto necesario para la deserialización de JSON
+    public AddressDTO() {
+    }
+
+    // Constructor que inicializa los campos desde la entidad Adress
     public AddressDTO(Adress adress) {
         this.id = adress.getId();
         this.nameStreet = adress.getNameStreet();
@@ -28,49 +27,70 @@ public class AddressDTO {
         this.typeHome = adress.getTypeHome();
         this.floorNumber = adress.getFloorNumber();
         this.aparmentNumber = adress.getAparmentNumber();
-
-//        this.clients = converClientsToDto(adress.getClients());
     }
 
-//    private List<ClientDTO> converClientsToDto(List<Client> clients) {
-//        return clients.stream()
-//                .map(ClientDTO::new)
-//                .collect(Collectors.toList());
-//    }
-
+    // Getters y setters
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNameStreet() {
         return nameStreet;
     }
 
+    public void setNameStreet(String nameStreet) {
+        this.nameStreet = nameStreet;
+    }
+
     public String getZipCode() {
         return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 
     public String getBetweenStreets() {
         return betweenStreets;
     }
 
+    public void setBetweenStreets(String betweenStreets) {
+        this.betweenStreets = betweenStreets;
+    }
+
     public int getStreetNumber() {
         return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
     }
 
     public TypeHome getTypeHome() {
         return typeHome;
     }
 
+    public void setTypeHome(TypeHome typeHome) {
+        this.typeHome = typeHome;
+    }
+
     public int getFloorNumber() {
         return floorNumber;
+    }
+
+    public void setFloorNumber(int floorNumber) {
+        this.floorNumber = floorNumber;
     }
 
     public String getAparmentNumber() {
         return aparmentNumber;
     }
 
-//    public List<ClientDTO> getClients() {
-//        return clients;
-//    }
+    public void setAparmentNumber(String aparmentNumber) {
+        this.aparmentNumber = aparmentNumber;
+    }
 }
--- src/main/java/mindhub/VoyagerRestaurante/models/Adress.java
+++ src/main/java/mindhub/VoyagerRestaurante/models/Adress.java
@@ -14,16 +14,33 @@ public class Adress {
     private String zipCode;
     private String betweenStreets;
     private int streetNumber;
+
+    @Enumerated(EnumType.STRING)  // Mapear el enum como String
     private TypeHome typeHome;
-    private int floorNumber;
+
+    @Column(nullable = true)  // Permitir que este campo sea nulo
+    private Integer floorNumber;
+
+    @Column(nullable = true)  // Permitir que este campo sea nulo
     private String aparmentNumber;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "client_id", nullable = false)
-    @JsonBackReference
     private Client client;
 
-    public Adress(String nameStreet, String betweenStreets, int streetNumber, TypeHome typeHome, int floorNumber, String aparmentNumber, String zipCode) {
+    // Constructor para HOUSE
+    public Adress(String nameStreet, String betweenStreets, int streetNumber, TypeHome typeHome, String zipCode) {
+        this.nameStreet = nameStreet;
+        this.betweenStreets = betweenStreets;
+        this.streetNumber = streetNumber;
+        this.typeHome = typeHome;
+        this.zipCode = zipCode;
+        this.floorNumber = null;  // Para una casa, no tiene sentido tener piso o departamento
+        this.aparmentNumber = null;
+    }
+
+    // Constructor para APARTMENT
+    public Adress(String nameStreet, String betweenStreets, int streetNumber, TypeHome typeHome, Integer floorNumber, String aparmentNumber, String zipCode) {
         this.nameStreet = nameStreet;
         this.betweenStreets = betweenStreets;
         this.streetNumber = streetNumber;
@@ -33,63 +50,7 @@ public class Adress {
         this.zipCode = zipCode;
     }
 
-    public Adress(String nameStreet, String betweenStreets, int streetNumber, TypeHome typeHome, String zipCode) {
-        this.nameStreet = nameStreet;
-        this.betweenStreets = betweenStreets;
-        this.streetNumber = streetNumber;
-        this.typeHome = typeHome;
-        this.zipCode = zipCode;
-    }
-
-    public Adress() {}
-
-    // Getters y setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNameStreet() {
-        return nameStreet;
-    }
-
-    public void setNameStreet(String nameStreet) {
-        this.nameStreet = nameStreet;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getBetweenStreets() {
-        return betweenStreets;
-    }
-
-    public void setBetweenStreets(String betweenStreets) {
-        this.betweenStreets = betweenStreets;
-    }
-
-    public int getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(int streetNumber) {
-        this.streetNumber = streetNumber;
-    }
-
-    public int getFloorNumber() {
-        return floorNumber;
-    }
-
-    public void setFloorNumber(int floorNumber) {
-        this.floorNumber = floorNumber;
+    public Adress() {
     }
 
     public TypeHome getTypeHome() {
@@ -98,6 +59,14 @@ public class Adress {
 
     public void setTypeHome(TypeHome typeHome) {
         this.typeHome = typeHome;
+    }
+
+    public Integer getFloorNumber() {
+        return floorNumber;
+    }
+
+    public void setFloorNumber(Integer floorNumber) {
+        this.floorNumber = floorNumber;
     }
 
     public String getAparmentNumber() {
