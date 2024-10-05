c44f01dcb76b5dfb21c5d62f39c2430012a421ff
franpaezl <franciscopaezlastra@gmail.com>
2024-10-05 15:24:16 -0300

    cambios Luz

---
+++ src/main/java/mindhub/VoyagerRestaurante/dtos/PaymentResponseDTO.java
@@ -0,0 +1,23 @@
+package mindhub.VoyagerRestaurante.dtos;
+
+public class PaymentResponseDTO {
+    private boolean success;
+    private String message;
+
+    // Getters y Setters
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
